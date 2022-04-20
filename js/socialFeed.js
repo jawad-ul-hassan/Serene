6c8a716f2ca068eb743b5ecc2edb292e468c02b4
jawad-ul-hassan <intelligentjawad@gmail.com>
2022-04-21 04:59:27 +0500

    all features added

--- js/socialFeed.js
+++ js/socialFeed.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js';
 import {
   query,
   getFirestore,
@@ -7,64 +7,64 @@ import {
   addDoc,
   getDocs,
   orderBy,
-} from "https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js";
+} from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCfr5JxEnZay7bcDSRSbod-BT9qJvv2IFQ",
-  authDomain: "serene-4161c.firebaseapp.com",
-  projectId: "serene-4161c",
-  storageBucket: "serene-4161c.appspot.com",
-  messagingSenderId: "1013532091693",
-  appId: "1:1013532091693:web:7a81eb71186ed251c25fa4",
+  apiKey: 'AIzaSyCfr5JxEnZay7bcDSRSbod-BT9qJvv2IFQ',
+  authDomain: 'serene-4161c.firebaseapp.com',
+  projectId: 'serene-4161c',
+  storageBucket: 'serene-4161c.appspot.com',
+  messagingSenderId: '1013532091693',
+  appId: '1:1013532091693:web:7a81eb71186ed251c25fa4',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const form = document.querySelector(".form");
-const postArea = document.querySelector(".post-area");
+const form = document.querySelector('.form');
+const postArea = document.querySelector('.post-area');
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', e => {
   e.preventDefault();
-  const post = document.querySelector("#post").value;
-  addDoc(collection(db, "posts"), {
-    user: localStorage.getItem("user"),
+  const post = document.querySelector('#post').value;
+  addDoc(collection(db, 'posts'), {
+    user: localStorage.getItem('user'),
     post: post,
     date: Date.now(),
   })
     .then(() => {
       // add post data to the top of the post area
-      const postEl = document.createElement("div");
-      postEl.classList.add("post");
+      const postEl = document.createElement('div');
+      postEl.classList.add('post');
       postEl.innerHTML = `
           <div class="post-header">
-            <div class="post-user"><b>${localStorage.getItem("user")}</b></div>
-            <div class="post-date">${new Date()}</div>
+            <div class="post-user"><b>${localStorage.getItem('user')}</b></div>
+            <div class="post-date">${new Date().toDateString()}</div>
           </div>
           <div class="post-body">${post}</div>
         `;
       postArea.prepend(postEl);
       form.reset();
     })
-    .catch((err) => {
+    .catch(err => {
       alert(err);
     });
 });
 
 // Get posts and display them in the post area
 const getPosts = async () => {
-  const q = query(collection(db, "posts"), orderBy("date", "desc"));
+  const q = query(collection(db, 'posts'), orderBy('date', 'desc'));
   const posts = await getDocs(q);
-  const snapshot = posts.docs.map((doc) => doc.data());
-  snapshot.forEach((post) => {
-    const postEl = document.createElement("div");
-    postEl.classList.add("post");
+  const snapshot = posts.docs.map(doc => doc.data());
+  snapshot.forEach(post => {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
     postEl.innerHTML = `
         <div class="post-header">
-          <div class="post-user"><b>${post.user}</b></div>
-          <div class="post-date">${new Date(post.date)}</div>
+          <div class="post-user"><h2>${post.user}</h2></div>
+          <div class="post-date">${new Date(post.date).toDateString()}</div>
         </div>
         <div class="post-body">${post.post}</div>
     `;
